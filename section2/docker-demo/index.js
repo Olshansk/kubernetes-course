d3f4c3925174b09e77826b602b07b8835eca9ca0
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-07-29 08:59:30 -0700

    Simple setup for a local nodejs server

---
+++ section2/docker-demo/index.js
@@ -0,0 +1,13 @@
+var expres = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hellow World!');
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
